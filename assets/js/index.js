8414d00bf5ac5e80fecda2b96a94cdaf87c06f3e
Vlad <vlad086367@gmail.com>
2025-08-30 22:56:01 +0300

    Add Event Loop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,21 @@
 'use strict';
 
-// Атрибути
 
-// const p1 = document.querySelector('#p1');
+console.log('before timeout');
+setTimeout(() => {
+    console.log('in timeout');
+}, 1000);
 
-// console.dir(p1.title);
-// console.dir(p1.id);
+console.log('ather timeout');
 
-// p1.title = 'New title';
+function f1() {
+    console.trace();
+    console.log('end');
+}
 
-// p1.hidden = true;
+function f() {
+    console.trace();
+    f1();
+}
 
-// p1.style.color = 'red';
-// p1.style.backgroundColor = 'orange';
-
-// //-------------------------------
-// const p2 = document.getElementById('p2');
-// p2.setAttribute('title', 'Title of p2');
-// p2.setAttribute('style', 'font-size: 80px; color: green;')
-// // p2.setAttribute('hidden', 'hidden');
-
-// p2.textContent = 'New text';
-
-
-// const bodyEl = document.querySelector('body').style.backgroundColor = 'green';
-
-// const imgAddres = {
-//     src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT74p0-GFNquBzzGLN9SazH22Wrus46aPPqYQ&s', 
-//     alt: 'Cat',
-// }
-
-// const imgEl = document.querySelector('img');
-// imgEl.src = imgAddres.src;
-// imgAddres.alt = imgAddres.alt;
-
-// imgEl.setAttribute('width', '150');
-
-
-// const obj = {
-//     number: 1,
-//     string: 'string',
-//     array: [1, 2, 3, true],
-//     boolean: true,
-//     null: null,
-//     object: {prop: 10},
-//     getNumber() {
-//         return this.number;
-//     },
-// };
-
-// // obj => JSON  сереалізація
-// const objJson = JSON.stringify(obj);
-
-// // JSON => obj  десереалізація
-// const parseJson = JSON.parse(objJson);
+f();
